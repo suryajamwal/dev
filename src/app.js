0850671be4b4e3e7316b0a85d5df7e1e4503e68b
surya <suryajamwal@143.com>
2025-08-12 12:14:03 +0530

    testing routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express=require("express");
 const app=express();
  
-app.use("/profile",(req,res) => {
-    res.send("userprofile is showing");
+
+app.post("/user",(req,res) =>{
+   res.send({firstname:"surya" ,lastname:"dev"});
+})
+app.get("/user",(req,res) => {
+    res.send("userprfile is showing");
+})
+
+app.delete("/user",(req,res)=>{
+   res.send("deleted successfully");
 })
  app.use((req,res) =>{
     res.send("server  is running");
- });
+ }); 
+
  app.listen(3000,() => {
     console.log("server is running on port number 3000 :")
  });
